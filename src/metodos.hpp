125cce60c9e8cb7d80eafd5f81d5fd2841bfdbe4
Ramos <gu.ramos@proton.me>
2024-12-31 15:14:54 -0300

    Reorganizei a estrutura dos arquivos

---
+++ src/metodos.hpp
@@ -0,0 +1,19 @@
+#ifndef METODOS_H
+#define METODOS_H
+
+#include <functional>
+#include <tuple>
+#include <cmath>
+
+// Usando o método "symmetric difference quotient" pra achar a derivada de uma função genérica
+// Esse método é inclusive usado em diversas calculadoras científicas com h = 0.001.
+// https://en.wikipedia.org/wiki/Numerical_differentiation
+double derivada(std::function<double(double)> funcao, double x);
+
+// Desenvolvendo o metodo de newton original, pedido pelo item a da questao
+std::tuple<double, double> newton_raphson(std::function<double(double)> funcao, double d0, double precisao, int max_iteracoes);
+
+// Desenvolvendo o metodo de newton original, pedido pelo item a da questao
+std::tuple<double, double> newton_raphson_modificado(std::function<double(double)> funcao, double d0, double precisao, double lambda, int max_iteracoes);
+
+#endif
